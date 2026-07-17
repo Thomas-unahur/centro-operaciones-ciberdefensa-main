43122da9ab5b972fdb3cbb747fd40f66e4654688
Thimo23 <thomasortuzar@gmail.com>
2026-07-17 19:32:21 -0300

    Se agrego la clase analista

---
+++ centro-operaciones-ciberdefensa-main/src/main/java/ar/edu/unahur/obj2/operaciones/Analistas/Analista.java
@@ -0,0 +1,37 @@
+package ar.edu.unahur.obj2.operaciones.Analistas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unahur.obj2.operaciones.AlertasDeSeguridad.Alerta;
+import ar.edu.unahur.obj2.operaciones.CriteriosDeTriaje.CriterioAlerta;
+
+public class Analista {
+    private final String legajo;
+    private CriterioAlerta criterioAlerta;
+    private List<Alerta> historialDeAlertas;
+   
+    public Analista(String legajo, CriterioAlerta criterioAlerta, List<Alerta> historialDeAlertas) {
+        this.legajo = legajo;
+        this.criterioAlerta = criterioAlerta;
+        this.historialDeAlertas = new ArrayList<>();
+    }
+
+    public List<Alerta> listasProcesadas(){
+        return this.historialDeAlertas;
+    }
+
+    public void setCriterioAlerta(CriterioAlerta criterioAlerta) {
+        this.criterioAlerta = criterioAlerta;
+    }
+
+    public String getLegajo() {
+        return legajo;
+    }
+
+    public CriterioAlerta getCriterioAlerta() {
+        return criterioAlerta;
+    }
+
+    
+}
